f0493196d665ff3b66078538525c94312c24d13a
Yaroslav Panyukov <ysp6@tpu.ru>
2024-11-03 17:28:06 +0700

    add Lesson2 homework

---
+++ Lesson2/Part1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int year;
+        Console.Write("Введите год: ");
+        while (!int.TryParse(Console.ReadLine(), out year) || year <= 0 || year >= 30000)
+        {
+            Console.Write("Год должен быть целым числом в диапазоне от 0 до 30000: ");
+        }
+        if(year % 4 == 0 && year % 100 != 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else if(year % 400 == 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }       
+    }
+}
---
+++ Lesson2/Part2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson2;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+        int amount;
+        decimal total = 0;
+        decimal expense;
+        Console.Write("Введите количество расходов: ");
+        while (!int.TryParse(Console.ReadLine(), out amount) || amount <= 0)
+        {
+            Console.Write("Количество расходов должно быть целым положительным числом: ");
+        }
+        for (int i = 1; i <= amount; i++)
+        {
+            Console.Write($"Введите сумму расхода {i}: ");
+            while (!decimal.TryParse(Console.ReadLine(), out expense) || expense < 0)
+            {
+                Console.Write("Сумма расхода должна быть положительным числом: ");
+            }
+            total += expense;
+        }
+        Console.WriteLine($"Общие расходы за месяц составляют: {total}. Очень много :(");
+    }
+}
