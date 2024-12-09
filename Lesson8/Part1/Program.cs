c7df2a71644d163d35e618404fed5c24c76ae601
Yaroslav Panyukov <ysp6@tpu.ru>
2024-12-09 19:14:36 +0700

    add lesson8 homework

---
+++ Lesson8/Part1/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Part1;
+
+using System;
+
+public delegate void NumberReachedEventHandler(int number);
+
+public class Counter
+{
+    // Событие, объявленное с использованием делегата
+    public event NumberReachedEventHandler NumberReached;
+
+    public void StartCounting()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(i);
+            if (i == 77)
+            {
+                // Вызываем событие
+                NumberReached?.Invoke(i);
+            }
+        }
+    }
+}
+
+public class Handler1
+{
+    public void OnNumberReached(int number)
+    {
+        Console.WriteLine("Пора действовать, ведь уже 77");
+    }
+}
+
+public class Handler2
+{
+    public void OnNumberReached(int number)
+    {
+        Console.WriteLine("Уже 77, давно пора было начать!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int rndHandler = rnd.Next(0, 10);
+        Counter counter = new Counter();
+        Handler1 handler1 = new Handler1();
+        Handler2 handler2 = new Handler2();
+
+        // Подписка на событие
+        if (rndHandler < 5)
+        {
+            counter.NumberReached += handler1.OnNumberReached;
+        }
+        else if (rndHandler > 4)
+        {
+            counter.NumberReached += handler2.OnNumberReached;
+        }
+
+        counter.StartCounting();
+    }
+}
+
---
+++ Lesson8/Part2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Part2;
+
+public delegate void PongEventHandler(string message);
+public delegate void PingEventHandler(string message);
+
+public class Ping
+{
+    private Random rnd = new Random();
+
+    // Событие, объявленное с использованием делегата
+    public event PongEventHandler Ponged;
+
+    public void SendPing()
+    {
+        int rndPing = rnd.Next(0, 10);
+        Console.WriteLine("Ping отправляет Pong");
+        if (rndPing != 3)
+        {
+            Ponged?.Invoke("Ping получил Pong");
+        }
+        else
+        {
+            Console.WriteLine("Ping промахнулся! Победил Pong");
+        }
+    }
+}
+
+public class Pong
+{
+    private Random rnd = new Random();
+
+    // Событие, объявленное с использованием делегата
+    public event PingEventHandler Pinged;
+
+    public void SendPong()
+    {
+        int rndPong = rnd.Next(0, 10);
+        Console.WriteLine("Pong отправляет Ping");
+        if (rndPong != 3)
+        {
+            Pinged?.Invoke("Pong получил Ping");
+        }
+        else
+        {
+            Console.WriteLine("Pong промахнулся! Победил Ping");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Ping ping = new Ping();
+        Pong pong = new Pong();
+
+        // Подписка на события
+        ping.Ponged += (message) => { Console.WriteLine(message); pong.SendPong(); };
+        pong.Pinged += (message) => { Console.WriteLine(message); ping.SendPing(); };
+
+        ping.SendPing();
+    }
+}
+
